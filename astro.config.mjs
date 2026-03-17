85ea3d26787c1ce13f8105eb211dc832995098e4
Leandro G. Almeida <leandro.g.almeida@gmail.com>
2026-03-16 18:15:22 -0700

    🔧 build: fix custom domain pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-const [githubOwner, githubRepo] = (process.env.GITHUB_REPOSITORY ?? '').split('/');
-const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
-
 export default defineConfig({
   output: 'static',
-  site: isGitHubActions && githubOwner ? `https://${githubOwner}.github.io` : 'https://terreaux.co',
-  base: isGitHubActions && githubRepo ? `/${githubRepo}` : '/',
+  site: 'https://studio.terreaux.co',
+  base: '/',
   integrations: [react(), tailwind()]
 });
